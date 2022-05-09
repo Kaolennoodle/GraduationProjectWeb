e1df863618d1e268725616986045e445aa9e870d
Kaolennoodle <993700226@qq.com>
2022-05-09 16:52:03 +0800

    调整了信息提示效果，修复了编辑用户信息窗口内头像无法显示的问题。

--- src/views/UserInterface.vue
+++ src/views/UserInterface.vue
@@ -28,59 +28,48 @@
     <!--        编辑个人资料窗口-->
     <el-dialog title="个人信息" :visible.sync="infoDialogVisible">
 
-      <el-row :gutter="20">
-        <el-col :span="6"><el-avatar :size="75" :src="user.uavatarPath"
-                                     style="margin-left: 50px">
-        </el-avatar></el-col>
-        <el-col :span="6">
-          <el-popover
-              placement="right"
-              width="206"
-              trigger="click"
-              style="margin-top: 23px">
-            <el-upload
-                class="avatar-uploader"
-                action="http://localhost:8081/file/upload"
-                :show-file-list="false"
-                :on-success="handleAvatarSuccess"
-                :before-upload="beforeAvatarUpload">
-              <img v-if="imageUrl" :src="imageUrl" class="avatar">
-              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-            </el-upload>
-            <el-button slot="reference">修改头像</el-button>
-          </el-popover>
-        </el-col>
-      </el-row>
-      <el-form :inline="true" label-width="80px" size="small" style="margin-top: 20px;">
-        <el-form-item label="姓名">
-          <el-input v-model="userInfo.uname" autocomplete="off" suffix-icon="el-icon-user"></el-input>
-        </el-form-item>
-        <el-form-item label="学号">
-          <el-input v-model="userInfo.ustuNum" autocomplete="off" suffix-icon="el-icon-s-custom"></el-input>
-        </el-form-item>
-        <el-form-item label="昵称">
-          <el-input v-model="userInfo.unickname" autocomplete="off" suffix-icon="el-icon-chat-dot-round"></el-input>
-        </el-form-item>
-        <el-form-item label="电话">
-          <el-input v-model="userInfo.uphone" autocomplete="off" suffix-icon="el-icon-mobile-phone"></el-input>
-        </el-form-item>
-        <el-form-item label="邮箱">
-          <el-input v-model="userInfo.uemail" autocomplete="off" suffix-icon="el-icon-message"></el-input>
-        </el-form-item>
-        <!--        <el-form-item label="用户类型">-->
-        <!--          <el-select v-model="form.utype" placeholder="用户类型">-->
-        <!--            <el-option-->
-        <!--                v-for="item in options2"-->
-        <!--                :key="item.value"-->
-        <!--                :label="item.label"-->
-        <!--                :value="item.value">-->
-        <!--            </el-option>-->
-        <!--          </el-select>-->
-        <!--        </el-form-item>-->
-        <el-form-item label="登录账号">
-          <el-input v-model="userInfo.uloginName" autocomplete="off" suffix-icon="el-icon-mouse"></el-input>
-        </el-form-item>
-      </el-form>
+      <div style="float: left; margin-left: 50px; margin-top: 40px">
+        <el-tooltip class="item" effect="light" content="点击上传新头像" placement="top">
+          <el-upload
+              class="avatar-uploader"
+              action="http://localhost:8081/file/upload"
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess"
+              :before-upload="beforeAvatarUpload">
+            <img v-if="imageUrl" :src="imageUrl" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-tooltip>
+      </div>
+
+      <div>
+        <el-form :inline="true" label-width="80px" size="small" style="margin-top: 20px; margin-left: 160px;">
+          <el-form-item label="昵称">
+            <el-input v-model="userInfo.unickname" autocomplete="off" suffix-icon="el-icon-chat-dot-round"
+                      style="width: 170px"></el-input>
+          </el-form-item>
+          <el-form-item label="姓名">
+            <el-input v-model="userInfo.uname" autocomplete="off" suffix-icon="el-icon-user"
+                      style="width: 170px"></el-input>
+          </el-form-item>
+          <el-form-item label="学号">
+            <el-input v-model="userInfo.ustuNum" autocomplete="off" suffix-icon="el-icon-s-custom"
+                      style="width: 170px"></el-input>
+          </el-form-item>
+          <el-form-item label="电话">
+            <el-input v-model="userInfo.uphone" autocomplete="off" suffix-icon="el-icon-mobile-phone"
+                      style="width: 170px"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱">
+            <el-input v-model="userInfo.uemail" autocomplete="off" suffix-icon="el-icon-message"
+                      style="width: 170px"></el-input>
+          </el-form-item>
+          <el-form-item label="登录账号">
+            <el-input v-model="userInfo.uloginName" autocomplete="off" suffix-icon="el-icon-mouse"
+                      style="width: 170px"></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
 
       <div slot="footer" class="dialog-footer">
         <el-button @click="handleDialogCancel">取 消</el-button>
@@ -107,25 +96,28 @@
 /*头像上传相关*/
 .avatar-uploader .el-upload {
   border: 1px dashed #d9d9d9;
-  border-radius: 6px;
+  border-radius: 10px;
   cursor: pointer;
   position: relative;
   overflow: hidden;
 }
+
 .avatar-uploader .el-upload:hover {
   border-color: #409EFF;
 }
+
 .avatar-uploader-icon {
   font-size: 28px;
   color: #8c939d;
-  width: 178px;
-  height: 178px;
-  line-height: 178px;
+  width: 100px;
+  height: 100px;
+  line-height: 100px;
   text-align: center;
 }
+
 .avatar {
-  width: 178px;
-  height: 178px;
+  width: 100px;
+  height: 100px;
   display: block;
 }
 </style>
@@ -158,11 +150,14 @@ export default {
     saveUserInfo() {
       request.post("/user", this.userInfo).then(res => {
         if (res) {
-          this.infoDialogVisible = false
-          this.$message.success({
-            showClose: true,
-            message: "保存成功！"
+          this.$notify({
+            title: '成功',
+            message: '您的信息已成功保存',
+            position: 'bottom-right',
+            duration: 2000,
+            type: 'success'
           })
+          this.infoDialogVisible = false
           this.user.uavatarPath = this.userInfo.uavatarPath
           this.user.unickname = this.userInfo.unickname
           this.user.uloginName = this.userInfo.uloginName
@@ -184,11 +179,13 @@ export default {
      * @param file
      */
     handleAvatarSuccess(res, file) {
-      this.imageUrl = URL.createObjectURL(file.raw);
+      this.imageUrl = res;
       this.userInfo.uavatarPath = this.imageUrl
       this.$notify({
         title: '头像上传成功',
         message: '点击确认保存头像',
+        position: 'bottom-right',
+        duration: 2000,
         type: 'success'
       });
     },
@@ -204,7 +201,7 @@ export default {
       if (!isLt2M) {
         this.$message.error('上传头像图片大小不能超过 2MB!');
       }
-      return isJPG && isLt2M;
+      return (isJPG || isPNG || isWEBP) && isLt2M;
     }
   },
   components: {
@@ -222,8 +219,8 @@ export default {
       }
     })
 
-    this.$bus.$on('editInfo', (data) => {
-      request.get("http://localhost:8081/user/page", {
+    this.$bus.$on('editInfo', async (data) => {
+      await request.get("http://localhost:8081/user/page", {
         params: {
           pageNum: 1,
           pageSize: 10,
@@ -232,6 +229,7 @@ export default {
       }).then(res => {
         this.userInfo = res.data.records[0]
       })
+      this.imageUrl = this.userInfo.uavatarPath
       this.infoDialogVisible = true
     })
   }
